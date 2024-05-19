b3df90e9b4c8dc38b8287a69b16ffcf19750f173
Michael Bestas <mkbestas@lineageos.org>
2024-05-20 01:45:16 +0300

    cheeseburger: Drop COMMON_LUNCH_CHOICES
    As of 14 QPR2 this is broken, let's remove it instead of updating it
    every 3 months. breakfast is the way to go.
    
    Change-Id: I097bd54b894d9a49c7ce8b1ab6f343349e268fd8

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,8 +6,3 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/sigma_cheeseburger.mk
-
-COMMON_LUNCH_CHOICES := \
-    sigma_cheeseburger-user \
-    sigma_cheeseburger-userdebug \
-    sigma_cheeseburger-eng
